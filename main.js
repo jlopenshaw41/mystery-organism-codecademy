466f2b45d0910f19b12f1235615df003f01a902f
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-10-28 16:53:08 +0000

    completed project

--- main.js
+++ main.js
@@ -19,11 +19,10 @@ const pAequorFactory = (number, array) => {
     dna: array,
     mutate: function () {
       let getRandomBaseIndex = [Math.floor(Math.random() * 14)];
-      console.log(this.dna[getRandomBaseIndex]);
       let newBase = returnRandBase();
+
       if (this.dna[getRandomBaseIndex] !== newBase) {
         this.dna[getRandomBaseIndex] = newBase;
-        console.log('Random');
       } else if (newBase === 'A') {
         this.dna[getRandomBaseIndex] = 'T';
       } else if (newBase === 'T') {
@@ -33,12 +32,62 @@ const pAequorFactory = (number, array) => {
       } else {
         this.dna[getRandomBaseIndex] = 'A';
       }
-      console.log(this.dna[getRandomBaseIndex]);
+    },
+    compareDNA: function (pAequor) {
+      let ex1 = this.dna;
+      let ex2 = pAequor.dna;
+      let noOfMatches = 0;
+
+      for (let i = 0; i < ex1.length; i++) {
+        if (ex1[i] === ex2[i]) {
+          noOfMatches++;
+        }
+      };
+      
+      let percentageMatch = (noOfMatches / 15 * 100).toFixed();
+
+      console.log(`Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentageMatch}% DNA in common`);
+    },
+    willLikelySurvive: function () {
+      let countCOrG = 0;
+      
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === 'C' || this.dna[i] === 'G') {
+          countCOrG++;
+        };
+      }
+      
+      if (((countCOrG / 15 * 100).toFixed()) >= 60) {
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 }
 
-console.log(pAequorFactory(1, mockUpStrand()).mutate());
+let create30Specimens = () => {
+  let specimenTotal = 0;
+  let specimenNum = 1;
+  let specimens = [];
+
+  for (let i = 0; specimenTotal < 30; i++) {
+    let currentSpecimen = pAequorFactory(specimenNum, mockUpStrand());
+    if (currentSpecimen.willLikelySurvive()) {
+      specimens.push(currentSpecimen);
+      specimenNum++;
+      specimenTotal++;
+    } else {
+      specimenNum++;
+    }
+  }
+} 
+
+create30Specimens();
+
+
+
+
 
 
 
